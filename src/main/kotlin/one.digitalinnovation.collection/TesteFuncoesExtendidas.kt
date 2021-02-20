d9e1dc65937828e97d4278afd42f7e62955af626
Gisele <gisesonia@gmail.com>
2021-02-20 10:24:30 -0300

    Funcoes Extendidas

---
+++ src/main/kotlin/one.digitalinnovation.collection/TesteFuncoesExtendidas.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal()
+    )
+
+    println("-------somatoria()---------")
+    println(salarios.somatoria())
+
+    println("-------media()---------")
+    println(salarios.media())
+}
